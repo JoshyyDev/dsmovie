7faad6da84cd9bf155bec757c0e151edee7a54f0
JoshyyDev <jrcbr321@gmail.com>
2022-05-12 19:41:21 -0300

    JoshyyDev

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar () {
           <a href="https://github.com/JoshyyDev">
             <div className='dsmovie-contact-container'>
               <GithubIcon />
-              <p className='dsmovie-contact-link'>/devsuperior</p>
+              <p className='dsmovie-contact-link'>JoshyyDev</p>
             </div>
           </a>  
         </div>
